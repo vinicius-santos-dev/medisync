9e4905e7ce700c9f5a96ce727ec0c74b4b53992b
Vinícius Santos <vinicius.ssantos.dev@gmail.com>
2025-01-10 15:42:27 -0300

    feat(appointment): implement admin dashboard with appointment infos and modals to cancel/schedule an appointment

--- components/forms/AppointmentForm.tsx
+++ components/forms/AppointmentForm.tsx
@@ -13,16 +13,24 @@ import { Doctors } from "@/constants";
 import { SelectItem } from "../ui/select";
 import Image from "next/image";
 import { FormFieldType } from "./PatientForm";
-import { createAppointment } from "@/lib/actions/appointment.actions";
+import {
+  createAppointment,
+  updateAppointment,
+} from "@/lib/actions/appointment.actions";
+import { Appointment } from "@/types/appwrite.types";
 
 const AppointmentForm = ({
   userId,
   patientId,
   type,
+  appointment,
+  setOpen,
 }: {
   userId: string;
   patientId: string;
   type: "create" | "cancel" | "schedule";
+  appointment?: Appointment;
+  setOpen: (open: boolean) => void;
 }) => {
   const router = useRouter();
 
@@ -33,32 +41,36 @@ const AppointmentForm = ({
   const form = useForm<z.infer<typeof AppointmentFormValidation>>({
     resolver: zodResolver(AppointmentFormValidation),
     defaultValues: {
-      primaryPhysician: "",
-      schedule: new Date(),
-      reason: "",
-      note: "",
-      cancellationReason: "",
+      primaryPhysician: appointment ? appointment.primaryPhysician : "",
+      schedule: appointment
+        ? new Date(appointment.schedule)
+        : new Date(Date.now()),
+      reason: appointment ? appointment.reason : "",
+      note: appointment ? appointment.note : "",
+      cancellationReason: appointment?.cancellationReason || "",
     },
   });
 
-  const onSubmit = async (values: z.infer<typeof AppointmentFormValidation>) => {
+  const onSubmit = async (
+    values: z.infer<typeof AppointmentFormValidation>
+  ) => {
     setIsLoading(true);
 
     let status;
 
     switch (type) {
-      case 'schedule':
-        status = 'scheduled';
+      case "schedule":
+        status = "scheduled";
         break;
-      case 'cancel':
-        status = 'cancelled';
+      case "cancel":
+        status = "cancelled";
         break;
       default:
-        status = 'pending';
+        status = "pending";
         break;
     }
     try {
-      if (type === 'create' && patientId) {
+      if (type === "create" && patientId) {
         const appointmentData = {
           userId,
           patient: patientId,
@@ -67,16 +79,37 @@ const AppointmentForm = ({
           reason: values.reason!,
           note: values.note,
           status: status as Status,
-        }
+        };
 
         const appointment = await createAppointment(appointmentData);
 
         if (appointment) {
           form.reset();
-          router.push(`/patients/${userId}/new-appointment/success?appointmentId=${appointment.$id}`);
+          router.push(
+            `/patients/${userId}/new-appointment/success?appointmentId=${appointment.$id}`
+          );
+        }
+      } else {
+        const appointmentToUpdate = {
+          userId,
+          appointmentId: appointment?.$id as string,
+          appointment: {
+            primaryPhysician: values?.primaryPhysician,
+            schedule: new Date(values?.schedule),
+            status: status as Status,
+            cancellationReason: values?.cancellationReason,
+          },
+          type,
+        };
+
+        const updatedAppointment = await updateAppointment(appointmentToUpdate);
+
+        if (updatedAppointment) {
+          if (setOpen) setOpen(false);
+
+          form.reset();
         }
       }
-
     } catch (error) {
       console.log(error);
     }
@@ -103,12 +136,14 @@ const AppointmentForm = ({
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
-        <section className="mb-12 space-y-4">
-          <h1 className="header">New Appointment 👋</h1>
-          <p className="text-dark-700">
-            Request a new appointment quickly and easily
-          </p>
-        </section>
+        {type === "create" && (
+          <section className="mb-12 space-y-4">
+            <h1 className="header">New Appointment 👋</h1>
+            <p className="text-dark-700">
+              Request a new appointment quickly and easily
+            </p>
+          </section>
+        )}
 
         {type !== "cancel" && (
           <>
